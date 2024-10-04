9a99c2ca14dea1381fe21def5d6aca54880158c9
Daria <Dasha-esha@mail.ru>
2024-10-04 17:38:19 +0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int creditSum = 1_000_000;
-        double loanPercent = 9.99;
-        int loanTermMonth = 36;
-        int monthPayment = service.calculate(creditSum, loanPercent, loanTermMonth);
+
+        int monthPayment = service.calculate(1_000_000, 9.99, 24);
         System.out.println(monthPayment);
 
 
